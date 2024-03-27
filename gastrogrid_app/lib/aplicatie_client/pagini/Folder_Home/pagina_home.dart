3d148c754416086f9ff264b8580a5a6ed8107058
Alexandru Ariton <aritonalexandru41@yahoo.com>
2024-03-27 15:35:38 +0200

    editare

--- gastrogrid_app/lib/aplicatie_client/pagini/Folder_Home/pagina_home.dart
+++ gastrogrid_app/lib/aplicatie_client/pagini/Folder_Home/pagina_home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, unnecessary_import, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
--- gastrogrid_app/lib/aplicatie_client/pagini/bara_navigare.dart
+++ gastrogrid_app/lib/aplicatie_client/pagini/bara_navigare.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_final_fields
+
 import 'package:flutter/material.dart';
 import 'package:gastrogrid_app/aplicatie_client/pagini/Folder_Home/pagina_home.dart';
 import 'package:gastrogrid_app/aplicatie_client/pagini/Folder_Orders/pagina_order.dart';
