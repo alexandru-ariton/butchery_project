3c92ae33c49910923af01b2747786b9f4e5bdcbe
Alexandru Ariton <aritonalexandru41@yahoo.com>
2024-04-06 14:12:58 +0300

    a

--- gastrogrid_app/lib/aplicatie_client/Folder_Home/componente_home/pagina_produs.dart
+++ gastrogrid_app/lib/aplicatie_client/Folder_Home/componente_home/pagina_produs.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables, library_private_types_in_public_api, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables, library_private_types_in_public_api, prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
 import 'package:gastrogrid_app/aplicatie_client/Folder_Home/componente_home/pagina_cart.dart';
