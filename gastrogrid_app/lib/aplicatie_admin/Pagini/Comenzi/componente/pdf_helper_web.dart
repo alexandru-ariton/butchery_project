6b428173f58fba07d6230011e805f89313020e7c
atiron28 <aritonalexandru41@yahoo.com>
2024-06-06 02:25:35 +0300

    a

---
+++ gastrogrid_app/lib/aplicatie_admin/Pagini/Comenzi/componente/pdf_helper_web.dart
@@ -0,0 +1,13 @@
+
+// ignore_for_file: unused_local_variable
+
+import 'dart:html' as html;
+
+void viewPdf(List<int> bytes) {
+  final blob = html.Blob([bytes], 'application/pdf');
+  final url = html.Url.createObjectUrlFromBlob(blob);
+  final anchor = html.AnchorElement(href: url)
+    ..setAttribute('target', '_blank')
+    ..click();
+  html.Url.revokeObjectUrl(url);
+}
